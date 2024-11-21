10dcd8149ca1087772ac7c05c6cfebd0c9274238
DannyVC123 <dannyjhuo@gmail.com>
2024-11-21 12:28:46 -0600

    finished up tkinter ui, still need to display album covers

--- client.py
+++ client.py
@@ -16,57 +16,56 @@ from spotify_client import SpotifyClient
 from track import Track
 from playlist import Playlist
 
-load_dotenv()
-client_id = os.getenv('CLIENT_ID')
-client_secret = os.getenv('CLIENT_SECRET')
-redirect_uri = os.getenv('REDIRECT_URI')
-user_id = os.getenv('USER_ID')
-auth_code = os.getenv('AUTH_CODE')
-
-def generate_authorization_url():
-    # https://developer.spotify.com/documentation/web-api/concepts/scopes
-    scopes = [
-        'user-read-recently-played',
-        'playlist-modify-public',
-        'playlist-modify-private'
-    ]
-    auth_url = (
-        f'https://accounts.spotify.com/authorize?response_type=code&client_id={client_id}'
-        f'&redirect_uri={redirect_uri}&scope={' '.join(scopes)}'
-    )
-    print("Visit this URL to authorize the app:", auth_url)
-    return auth_url
-
-def get_token():
-    # http://localhost:8080/?code=AQA9aROfylyTWis9bWy3oxgBiwFjcZlXRWMSCQj3hE1q78vLXs0qZTzoEOUPkoHrIVySwhBMLIsaK8JAFu1uCvU915EPp3lRoWnN7pw18Kff0dF2rzRzxuU-gEZ-weuo6wzkna_g2gQ44Ls3ohq7bSpYxf1Uw_8qtxaZtclTb5RGMUuTis4QXgL9gOFA7HHk8ijsuuDN0vDSQ3JQ5Myh5myMYle1eiQV0nCHOTA_UJLKhL6sxt1Gbfxyi56viHHi92gsjQI
-    callback_url = input('url >>> ')
-    auth_code = callback_url.split("code=")[1]
-
-    auth_str = f'{client_id}:{client_secret}'
-    auth_base64 = base64.b64encode(auth_str.encode('utf-8')).decode('utf-8')
-
-    token_url = 'https://accounts.spotify.com/api/token'
-    headers = {
-        'Authorization': f'Basic {auth_base64}',
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
-
-    data = {
-        'grant_type': 'authorization_code',
-        'code': auth_code,
-        'redirect_uri': redirect_uri
-    }
-
-    response = WebService.call(token_url, 'post', headers=headers, data=data)
+class Client:
+    def __init__(self, client_id, client_secret, redirect_uri):
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.redirect_uri = redirect_uri
     
-    if response.status_code == 200:
-        response_body = response.json()
-        access_token = response_body.get('access_token')
-        # refresh_token = response_body.get('refresh_token')  # Store for later use
-        return access_token
-    else:
-        print("Error:", response.json())
-        return None
+    def autorize(self):
+        # https://developer.spotify.com/documentation/web-api/concepts/scopes
+        scopes = [
+            'user-read-recently-played',
+            'playlist-modify-public',
+            'playlist-modify-private'
+        ]
+        auth_url = (
+            f'https://accounts.spotify.com/authorize?response_type=code&client_id={self.client_id}'
+            f'&redirect_uri={self.redirect_uri}&scope={' '.join(scopes)}'
+        )
+        # print("Visit this URL to authorize the app:", auth_url)
+        webbrowser.open(auth_url)
+
+    def get_token(self, callback_url):
+        # http://localhost:8080/?code=AQA9aROfylyTWis9bWy3oxgBiwFjcZlXRWMSCQj3hE1q78vLXs0qZTzoEOUPkoHrIVySwhBMLIsaK8JAFu1uCvU915EPp3lRoWnN7pw18Kff0dF2rzRzxuU-gEZ-weuo6wzkna_g2gQ44Ls3ohq7bSpYxf1Uw_8qtxaZtclTb5RGMUuTis4QXgL9gOFA7HHk8ijsuuDN0vDSQ3JQ5Myh5myMYle1eiQV0nCHOTA_UJLKhL6sxt1Gbfxyi56viHHi92gsjQI
+        # callback_url = input('url >>> ')
+        auth_code = callback_url.split("code=")[1]
+
+        auth_str = f'{self.client_id}:{self.client_secret}'
+        auth_base64 = base64.b64encode(auth_str.encode('utf-8')).decode('utf-8')
+
+        token_url = 'https://accounts.spotify.com/api/token'
+        headers = {
+            'Authorization': f'Basic {auth_base64}',
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+
+        data = {
+            'grant_type': 'authorization_code',
+            'code': auth_code,
+            'redirect_uri': self.redirect_uri
+        }
+
+        response = WebService.call(token_url, 'post', headers=headers, data=data)
+        
+        if response.status_code == 200:
+            response_body = response.json()
+            access_token = response_body.get('access_token')
+            # refresh_token = response_body.get('refresh_token')  # Store for later use
+            return access_token
+        else:
+            print("Error:", response.json())
+            return None
 
 '''
 def exchange_code_for_tokens(auth_code):
@@ -114,6 +113,7 @@ def refresh_access_token(refresh_token):
     return new_access_token
 '''
 
+'''
 auth_url = generate_authorization_url()
 webbrowser.open(auth_url)
 access_token = get_token()
@@ -129,14 +129,15 @@ spotify_client = SpotifyClient(access_token, user_id)
 
 # last_played_tracks = spotify_client.get_last_played_tracks()
 
-params_json = '''{
+params_json = """{
   "limit": 10,
   "seed_artists": ["fuslie", "lilypichu"]
 }
-'''
+"""
 recommended_tracks = spotify_client.get_track_recommendations(params_json)
 
 if recommended_tracks:
     playlist = spotify_client.create_playlist('lily and fuslie chat gpt playlist')
     playlist_url = spotify_client.populate_playlist(playlist, recommended_tracks)
     webbrowser.open(playlist_url)
+'''
--- spotify_gui.py
+++ spotify_gui.py
@@ -1,9 +1,34 @@
+import json
+import requests
+import webbrowser
+
+from dotenv import load_dotenv
+import uuid
+import pathlib
+import logging
+import sys
+import os
+import base64
+import time
 import tkinter as tk
+
+from web_service import WebService
+from client import Client
+from spotify_client import SpotifyClient
+from track import Track
+from playlist import Playlist
 
 class SpotifyGUI:
     width, height = 800, 800
 
     def __init__(self):
+        load_dotenv()
+        self.client_id = os.getenv('CLIENT_ID')
+        self.client_secret = os.getenv('CLIENT_SECRET')
+        self.redirect_uri = os.getenv('REDIRECT_URI')
+        self.user_id = os.getenv('USER_ID')
+        self.client = Client(self.client_id, self.client_secret, self.redirect_uri)
+
         self.create_ui()
         self.root.mainloop()
     
@@ -17,58 +42,81 @@ class SpotifyGUI:
         title_label = tk.Label(self.root, text='Spotify Playlist Generator', font=('Arial', 16, 'bold'))
         title_label.pack(pady=10)
 
-        # Login
-        frame = tk.Frame(self.root)
-        frame.pack(pady=10)
-        login_label = tk.Label(self.root, text='Playlist JSON from ChatGPT:', font=('Arial', 16, 'bold'))
+        # Spacer
+        spacer = tk.Label(self.root)
+        spacer.pack(pady=5)
 
+        # Frame for login section
+        login_frame = tk.Frame(self.root)
+        login_frame.pack(pady=10)
+
+        # Login Label and Button
+        login_label = tk.Label(login_frame, text='Click Here to Login:', font=('Arial', 16, 'bold'))
+        login_label.grid(row=0, column=0, padx=5, pady=5)
+        
+        login_button = tk.Button(login_frame, text='Login to Spotify', width=15, command=self.client.autorize)
+        login_button.grid(row=0, column=1, padx=5, pady=5)
+
+        # Autorization Code
+        callback_url_label = tk.Label(self.root, text='Paste Callback URL:', font=('Arial', 16, 'bold'))
+        callback_url_label.pack(pady=5)
+
+        self.callback_url_textbox = tk.Text(self.root, width=80, height=2, font=('Arial', 16))
+        self.callback_url_textbox.pack(pady=5)
+
+        submit_url_button = tk.Button(self.root, text='Submit URL', width=15, command=self.initialize_spotify_client)
+        submit_url_button.pack(pady=5)
+
+        # Spacer
+        spacer2 = tk.Label(self.root)
+        spacer2.pack(pady=5)
 
         # Playlist Prompt
-        playlist_prompt_label = tk.Label(self.root, text='Playlist JSON from ChatGPT:', font=('Arial', 16, 'bold'))
+        playlist_prompt_label = tk.Label(self.root, text='Paste Playlist JSON from ChatGPT:', font=('Arial', 16, 'bold'))
         playlist_prompt_label.pack(pady=5)
-        self.playlist_prompt_textbox = tk.Text(self.root, width=50, height=5, font=('Arial', 16), wrap=tk.WORD)
+
+        self.playlist_prompt_textbox = tk.Text(self.root, width=50, height=8, font=('Arial', 16), wrap=tk.WORD)
         self.playlist_prompt_textbox.pack(pady=5)
 
-        # Frame
-        frame = tk.Frame(self.root)
-        frame.pack(pady=10)
+        # Frame for number of songs and playlist name
+        songs_frame = tk.Frame(self.root)
+        songs_frame.pack(pady=10)
 
-        # Num Songs
-        num_songs_label = tk.Label(frame, text='Number of Songs (Default 10):', font=('Arial', 16, 'bold'))
-        num_songs_label.grid(row=0, column=0, padx=5, pady=5)
-        self.num_songs_textbox = tk.Entry(frame, width=5)
-        self.num_songs_textbox.grid(row=0, column=1, padx=5, pady=5)
-
-        '''
-        label2 = tk.Label(self.root, text='Last Name:')
-        label2.pack(pady=5)
-        entry2 = tk.Entry(self.root, width=30)
-        entry2.pack(pady=5)
-
-        label3 = tk.Label(self.root, text='Email:')
-        label3.pack(pady=5)
-        entry3 = tk.Entry(self.root, width=30)
-        entry3.pack(pady=5)
-
-        # Create buttons
-        submit_button = tk.Button(self.root, text='Submit', width=15)
-        submit_button.pack(pady=10)
-
-        cancel_button = tk.Button(self.root, text='Cancel', width=15)
-        cancel_button.pack(pady=5)
-        '''
-
-        '''
-        self.create_scale('Focal Length', 500, 3000, 1, frame = param_frame)
-        titles = ['X-Rotation', 'Y-Rotation', 'Z-Rotation']
-        self.rotation_scales = []
-        for i in range(3):
-            _, v = self.create_scale(titles[i], 0, 360, i+2, i, frame = param_frame)
-            self.rotation_scales.append([v, 0])
+        # Num Songs Label and Textbox
+        num_songs_label = tk.Label(songs_frame, text='Number of Songs in Playlist (Default 10):', font=('Arial', 16, 'bold'))
+        num_songs_label.grid(row=0, column=0, padx=5, pady=5, sticky=tk.W)
         
-        self.img = PhotoImage(file = './images/axes_reference_rh_small.png')
-        label = Label(self.window, image = self.img)
-        label.place(relx = 1.0, rely = 0.0, anchor = NE)
-        '''
+        self.num_songs_textbox = tk.Entry(songs_frame, width=5)
+        self.num_songs_textbox.grid(row=0, column=1, padx=5, pady=5, sticky=tk.W)
+
+        # Playlist Name
+        playlist_name_label = tk.Label(songs_frame, text='Playlist Name (ChatGPT will generate a name if empty):', font=('Arial', 16, 'bold'))
+        playlist_name_label.grid(row=1, column=0, padx=5, pady=5, sticky=tk.W)
+        
+        self.playlist_name_textbox = tk.Entry(songs_frame, width=15)
+        self.playlist_name_textbox.grid(row=1, column=1, padx=5, pady=5, sticky=tk.W)
+
+        # Submit button
+        submit_prompt_button = tk.Button(self.root, text='Generate Playlist', width=15, command=self.generate_playlist)
+        submit_prompt_button.pack(pady=5)
+    
+    def initialize_spotify_client(self):
+        callback_url = self.callback_url_textbox.get('1.0', 'end-1c')
+        if not callback_url:
+            return
+        access_token = self.client.get_token(callback_url)
+        print(access_token)
+
+        self.spotify_client = SpotifyClient(access_token, self.user_id)
+    
+    def generate_playlist(self):
+        params_json = self.playlist_prompt_textbox.get('1.0', 'end-1c')
+        recommended_tracks = self.spotify_client.get_track_recommendations(params_json)
+        if recommended_tracks:
+            playlist_name = self.playlist_name_textbox.get()
+            playlist = self.spotify_client.create_playlist(playlist_name)
+            playlist_url = self.spotify_client.populate_playlist(playlist, recommended_tracks)
+            webbrowser.open(playlist_url)
+
 
 SpotifyGUI()
